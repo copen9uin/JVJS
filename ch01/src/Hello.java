20740d2c9833b795f24ae5a4a810b48b97a9d178
copen9uin <copen9uin@gmail.com>
2022-08-11 03:21:21 +0900

    1

---
+++ ch01/src/Hello.java
@@ -0,0 +1,27 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ctrl + shift + L		단축키 전체 목록보기
+		// ctrl + + or -		글자 크기
+		// ctrl + d				한 줄 삭제
+		// ctrl + alt + down	행 단위 복사
+		// alt + shift + a		멀티컬럼 편집
+		// alt + up , down		행 단위 이동
+		// ctrl + i				자동 들여쓰기
+		// ctrl + /				주석(토글)
+		// ctrl + space			자동 완성
+		/*	여러 줄 주석	*/
+   		System.out.println("Hello, world");
+		System.out.println("Hello, world");
+		System.out.println("Hello, world");
+		System.out.println("Hello, world");
+		System.out.println("Hello, world");
+		System.out.println("Hello, world");
+		System.out.println("Hello, world");
+	}
+ 
+}
+//
+ 
