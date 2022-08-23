2d7db576eb4975d9393671afebe8f1c5794592b5
copen9uin <copen9uin@gmail.com>
2022-08-23 22:03:59 +0900

    0823

---
+++ ch02/src/Overflow.java
@@ -0,0 +1,31 @@
+
+public class Overflow {
+
+	public static void main(String[] args) {
+		// 정수의 오버플로우
+		
+		short sMin = -32768,	sMax = 32767;
+		char cMin = 0,	cMax = 65535;
+		
+		System.out.println("sMin = " + sMin);
+		System.out.println("sMin-1 = " + (short)(sMin-1));
+		System.out.println("sMax = " + sMax);
+		System.out.println("sMax+1 = " + (short)(sMax+1));
+		System.out.println("cMin = " + (int)cMin);
+		System.out.println("cMin-1 = " + (int)--cMin);
+		System.out.println("cMax = " + (int)cMax);
+		System.out.println("cMax+1 = " + (int)++cMax);
+		
+//		sMin = -32768
+//		sMin-1 = 32767
+//		sMax = 32767
+//		sMax+1 = -32768
+//		cMin = 0
+//		cMin-1 = 65535
+//		cMax = 65535
+//		cMax+1 = 0
+
+
+	}
+
+}
