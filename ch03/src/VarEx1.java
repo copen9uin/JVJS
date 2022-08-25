2c19c4c0b1f10c8de0ded46e54b75b2f87eecfab
copen9uin <copen9uin@gmail.com>
2022-08-25 21:57:15 +0900

    0825

--- ch03/src/VarEx1.java
+++ ch03/src/VarEx1.java
@@ -31,6 +31,10 @@ public class VarEx1 {
 //		byte b1 = i;	i는 리터럴이 아니라 변수이기 때문에 자동형변환 불가능
 		byte b1 = (byte)i;	//수동 형변환은 가능
 		System.out.println(b1);
+
+//		byte b2 = 1000;		byte의 범위를 벗어나기 때문에 오류
+		byte b2 = (byte)1000;	//에러는 발생하지 않지만 값손실이 발생
+		System.out.println(b2);
 		
 	}
 
